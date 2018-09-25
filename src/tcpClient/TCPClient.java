0e4beba5d3afbd8dc39ec7d894017dac707b0d5b
Tobias Valbjørn <tvalbjoern@gmail.com>
2018-09-25 12:19:58 +0200

    Implemented exception handling. Added functionality to close down the client.

--- src/tcpClient/TCPClient.java
+++ src/tcpClient/TCPClient.java
@@ -7,17 +7,40 @@ class TCPClient {
 	public static void main(String argv[]) throws Exception {
 		String sentence;
 		String modifiedSentence;
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		Socket clientSocket = new Socket("localhost", 12900);
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-		while (true) {
-			sentence = inFromUser.readLine();
-			outToServer.writeBytes(sentence + '\n');
-			modifiedSentence = inFromServer.readLine();
-			System.out.println("FROM SERVER: " + modifiedSentence);
+		final BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		final Socket clientSocket = new Socket("localhost", 12900);
+		final DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		final BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		try {
+			while (true) {
+				sentence = inFromUser.readLine();
+				outToServer.writeBytes(sentence + '\n');
+				modifiedSentence = inFromServer.readLine();
+				if (modifiedSentence == null) {
+					System.out.println("Connection closed\n");
+					break;
+				} else {
+					System.out.println("FROM SERVER: " + modifiedSentence);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				inFromUser.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				outToServer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				clientSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-
 	}
 }
